507c976b8b8fac14107d7153921bbfdf836300e6
Hyeonae <aeae_1@naver.com>
2023-03-01 18:56:50 +0900

    Chapter 3. 카프카 기본 개념 코드 (~카프카 스트림즈)

---
+++ ch3/kstreams-ktable-join/src/main/java/com/example/kstreamsktablejoin/KstreamsKtableJoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.kstreamsktablejoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstreamsKtableJoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstreamsKtableJoinApplication.class, args);
+	}
+
+}
---
+++ ch3/simple-kafka-streams/src/test/java/com/example/simplekafkastreams/SimpleKafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplekafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
